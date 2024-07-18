7700d1d8417c7a8e98b4d6ee806319af7359e8d2
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-07-18 13:58:06 +0800

    added route error catch

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 import { CoffeeGetDto } from 'shared-types';
@@ -11,11 +18,19 @@ export class AppController {
   @Get('/getCoffee/:id')
   @CacheTTL(0)
   async getCoffee(@Param('id') id: string): Promise<CoffeeGetDto> {
-    const [coffee, image] = await Promise.all([
-      this.appService.getCoffee(id),
-      this.appService.getImage(id),
-    ]);
+    try {
+      const [coffee, image] = await Promise.all([
+        this.appService.getCoffee(id),
+        this.appService.getImage(id),
+      ]);
 
-    return { ...coffee, image };
+      return { ...coffee, image };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'External service is unavailable',
+        HttpStatus.SERVICE_UNAVAILABLE,
+      );
+    }
   }
 }
