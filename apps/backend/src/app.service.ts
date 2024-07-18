d7acb98c7fd474c25a05acb6065555c74defc7d8
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-07-18 14:04:48 +0800

    refactor fetch error handling

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -5,11 +5,19 @@ export class AppService {
   getCoffee(id: string): Promise<CoffeeDataDto> {
     return fetch(
       `https://random-data-api.com/api/coffee/random_coffee?id=${id}`,
-    ).then((r) => r.json());
+    ).then(this.returnResponseJsonOrThrow);
   }
   getImage(id: string): Promise<ImageDataDto> {
     return fetch(
       `https://loremflickr.com/json/500/500/coffee,bean?id=${id}`,
-    ).then((r) => r.json());
+    ).then(this.returnResponseJsonOrThrow);
+  }
+  private async returnResponseJsonOrThrow(r: Response) {
+    if (!r.ok) {
+      const resText = await r.text();
+      console.log(resText);
+      throw new Error(`HTTP error! status: ${r.status}`);
+    }
+    return r.json();
   }
 }
