6f9f8b709f93671ba8fbda427cdbe18350a0bc7e
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-07-18 14:53:39 +0800

    added auto scroll + fixed button

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import CoffeeCard from "$lib/components/CoffeeCard.svelte";
   import { coffeeStore } from "$lib/stores";
+  import { tick } from "svelte";
 
   const items = coffeeStore.coffeeItems;
   const isLoading = coffeeStore.isLoading;
+
+  async function loadMoreClickhandler() {
+    await coffeeStore.getNextCoffee();
+    setTimeout(() => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    }, 150);
+  }
 </script>
 
 <svelte:head>
@@ -14,16 +22,17 @@
 <section class="flex flex-col gap-4">
   <h1 class="text-xl font-bold">Cofeeshop</h1>
 
-  <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  <div
+    class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-[80px]"
+  >
     {#each $items as item}
       <CoffeeCard {item} />
     {/each}
   </div>
   <button
-    class="rounded px-3 py-1 border-solid border border-black shadow hover:bg-black hover:text-white transition disabled:opacity-50"
+    class="fixed bottom-10 z-30 rounded px-3 py-1 border-solid border border-black shadow hover:bg-black hover:text-white transition disabled:opacity-50"
     disabled={$isLoading}
-    on:click={() => coffeeStore.getNextCoffee()}
-    >{$isLoading ? "Loading..." : "Load one more"}</button
+    on:click={loadMoreClickhandler}>{$isLoading ? "Loading..." : "More"}</button
   >
 </section>
 
